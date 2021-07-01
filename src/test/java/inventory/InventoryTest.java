ac97a713bb659a419adfaeb699e32685d6e5ab00
AJAY <ajayagrawal761999@gmail.com>
2021-07-02 01:56:49 +0530

    third

---
+++ src/test/java/inventory/InventoryTest.java
@@ -0,0 +1,30 @@
+package inventory;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryTest {
+    @Test
+    public void remove_product_in_inventory(){
+        Inventory inv = new Inventory();
+        Product pr =  new Product(4,"Grapes", BigDecimal.valueOf(100));
+        Product removedProduct = inv.removeProductFromInventory(pr);
+        Product removedProduct1 = inv.removeProductFromInventory(pr);
+        Product removedProduct2 = inv.removeProductFromInventory(pr);
+        Product removedProduct3 = inv.removeProductFromInventory(pr);
+        Product removedProduct4 = inv.removeProductFromInventory(pr);
+        //inv.addProductInInventory(pr);
+        inv.showInventory();
+        System.out.println(removedProduct3);
+    }
+    @Test
+    public void add_product_in_inventory(){
+        Inventory inv = new Inventory();
+        Product pr =  new Product(4,"Grapes", BigDecimal.valueOf(100));
+        inv.addProductInInventory(pr);
+        inv.showInventory();
+    }
+}
