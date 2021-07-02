c6a4102ea6a1cc27d12a4a14fcf6c5135990bb01
AJAY <ajayagrawal761999@gmail.com>
2021-07-02 09:59:31 +0530

    third

--- src/test/java/service/CartServiceTest.java
+++ src/test/java/service/CartServiceTest.java
@@ -1,53 +1,25 @@
 package service;
 
+import cart.ShoppingCart;
 import inventory.Inventory;
 import inventory.Product;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 class CartServiceTest {
-    /*@Test
+    @Test
     public void add_product_should_return_matched_product(){
         CartService cs = new CartService();
-        cs.addProduct(1);
-        Assertions.assertNotNull(cs.showCart());
-        cs.addProduct(1);
-        cs.addProduct(2);
-        cs.addProduct(3);
-        cs.addProduct(4);
-        cs.addProduct(4);
 
-        for (Integer item:cs.showCart().keySet()
-        ) {
-            System.out.println(Inventory.products
-                    .stream()
-                    .filter(product -> product.getBarcode().equals(item))
-                    .findFirst()
-                    .orElseThrow(
-                            () -> new RuntimeException("Product not found")
-                    ).getName()+"----"+cs.showCart().get(item)
-            );
-        }
-
-        cs.removeProduct(4);
-        cs.removeProduct(4);
-        cs.removeProduct(4);
-
-        //Assertions.assertEquals(Inventory.products.get(0).getBarcode(),cs.showCart().keySet().contains(1));
-        for (Integer item:cs.showCart().keySet()
-             ) {
-            System.out.println(Inventory.products
-                    .stream()
-                    .filter(product -> product.getBarcode().equals(item))
-                    .findFirst()
-                    .orElseThrow(
-                            () -> new RuntimeException("Product not found")
-                    ).getName()+"----"+cs.showCart().get(item)
-            );
-        }
-    }*/
+        Product pr =  new Product(4,"Grapes", BigDecimal.valueOf(100));
+        cs.addProduct(pr);
+        cs.showCart();
+        cs.removeProduct(pr);
+        cs.showCart();
+    }
 
 }
